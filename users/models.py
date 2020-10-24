c15546c18e32de8b324194b684066dd803ed6102
tomito26 <tommybwah@gmail.com>
2020-10-24 09:53:14 +0300

    made changes on the  profile model

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     image = CloudinaryField('profile-photo')
     
     def __str__(self):
-        return f'{self.username}Profile'
+        return f'{self.user.username} Profile'
