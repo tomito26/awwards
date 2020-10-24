ce9e88192328f0a622185616354df025d196a3cd
tomito26 <tommybwah@gmail.com>
2020-10-24 08:26:01 +0300

    updated the navbar and also linked the logout and login templates on the navbar

--- awward/settings.py
+++ awward/settings.py
@@ -142,6 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
