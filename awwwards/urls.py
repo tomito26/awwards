a3dffcf95995e487a87cebacfaf828b23a376fbb
tomito26 <tommybwah@gmail.com>
2020-10-25 17:38:00 +0300

    linked the posts to the Users

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -1,13 +1,14 @@
 from django.db.models.query import Prefetch
 from django.urls import path
 from . import views
-from .views import ProjectDeleteView, ProjectListView,ProjectDetailView,ProjectCreateView, ProjectUpdateView
+from .views import ProjectDeleteView, ProjectListView,ProjectDetailView,ProjectCreateView, ProjectUpdateView,UserProjectListView
 
 
 urlpatterns = [
     path('',ProjectListView.as_view(),name='home'),
+    path('user/<str:username>/',UserProjectListView.as_view(),name='user-projects'),
     path('project/<int:pk>/',ProjectDetailView.as_view(),name='project-detail'),
     path('project/new/',ProjectCreateView.as_view(), name='project-create'),
     path('project/<int:pk>/update/',ProjectUpdateView.as_view(),name='project-update'),
-    path('post/<int:pk>/delete',ProjectDeleteView.as_view(), name='project-delete')
+    path('project/<int:pk>/delete',ProjectDeleteView.as_view(), name='project-delete')
 ]
--- awwwards/views.py
+++ awwwards/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,get_object_or_404
 from .models import Project
+from django.contrib.auth.models import User
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
@@ -13,6 +14,17 @@ class ProjectListView(ListView):
     ordering = ['-date_posted']
 
 
+class UserProjectListView(ListView):
+    model = Project
+    template_name = 'awwwards/user_projects.html'
+    context_object_name = 'projects'
+   
+    
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get('username'))
+        return Project.objects.filter(author=user).order_by('-date_posted')
+        
+        
 class ProjectDetailView(DetailView):
     model = Project
     template_name = 'awwwards/project_detail.html'
@@ -34,20 +46,21 @@ class ProjectUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     def test_func(self):
         project= self.get_object()
         if self.request.user == project.author:
             return True
-        
+
         return False
 
 class ProjectDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Project
-    
+    success_url='/'
+
     def test_func(self):
         project = self.get_object()
         if self.request.user == project.author:
             return True
-        
+
         return False
