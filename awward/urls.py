52617ec4cc1413ba62e863db2d3da48e283b1cfa
tomito26 <tommybwah@gmail.com>
2020-10-25 03:31:05 +0300

    added landing_page to the homepage

--- awward/urls.py
+++ awward/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import awwwards
 from os import name
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
@@ -21,10 +22,12 @@ from django.urls.conf import include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('profile/',user_views.profile,name='profile'),
+    path('',include('awwwards.urls')),
 ]
